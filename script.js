d40a8f7d25b815a16b06864993f62ebe57a09e18
utkubilge <jasonanaminus@gmail.com>
2022-11-07 04:10:52 +0300

    refactored

--- script.js
+++ script.js
@@ -1,58 +1,40 @@
+let canvas;
+let ctx;
+let img;
+let gid = null;
+
 function onLoad() {
-    const canvas = document.getElementById("canvas1");
-    const ctx = canvas.getContext("2d");
+    canvas = document.getElementById("canvas1");
+    ctx = canvas.getContext("2d");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    const img = new Image();
+    img = new Image();
     img.src = 'tbh.png';
-    img.onload = () => { ctx.drawImage(img, canvas.width / 2 - 500 / 2, canvas.height / 2 - 500 / 2) }
+    img.onload = () => { ctx.drawImage(img, canvas.width / 2 - img.width / 2, canvas.height / 2 - img.height / 2) }
 }
 
-function random(from, to) {
-    return Math.floor(Math.random() * (to - from + 1)) + from;
-}
-
-function rng(from, to) {
-    let rngSet = []
-    for (let index = from; index < to + 1; index++) {
-        rngSet.push({ number: index, count: 0 })
-    }
-    console.log(rngSet)
-
-    for (let index = 0; index < 100; index++) {
-        rngSet[random(from, to)].count++;
-    }
-    console.log(rngSet)
-}
-
-function animation() {
-    const canvas = document.getElementById("canvas1");
-    const ctx = canvas.getContext("2d");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
+function animation(special_color = "50%") {
     class Particle {
         constructor() {
             this.x = canvas.width / 2;
-            this.y = canvas.height / 2;
+            this.y = canvas.height / 2 - 150;
             this.size = 8;
             this.vx = (Math.random() * 10 + 10) * (Math.random() * 2 - 1);
             this.vy = (Math.random() * 10 + 10) * (Math.random() * 2 - 1);
-            this.color = Math.floor(Math.random() * 16777215).toString(16);
-            this.veltime = 50;
+            this.color = Math.floor(Math.random() * 360);
+
         }
         draw(context) {
-            context.fillStyle = "#" + this.color;
-            context.fillRect(this.x, this.y, this.size, this.size)
+            if(this.y > canvas.height ) {
+                return;
+            }
+            context.fillStyle = `hsl(${this.color}, 100%, ${special_color})`;
+            context.fillRect(this.x, this.y, this.size, this.size);
         }
         update() {
-            if (this.veltime-- > 0) {
-                this.y += this.vy;
-                this.x += this.vx;
-            }
-
-            else
-                this.y += 5;
+            this.y += this.vy;
+            this.x += this.vx;
+            this.vy += 0.5;
         }
     }
 
@@ -63,7 +45,6 @@ function animation() {
             this.amount = amount;
             this.particlesArray = [];
             this.init();
-            this.vanishtime = 100;
         }
         init() {
             for (let index = 0; index < this.amount; index++) {
@@ -71,33 +52,45 @@ function animation() {
             }
         }
         draw(context) {
+            let limit = 0;
             this.particlesArray.forEach(e => e.draw(context))
+            this.particlesArray.forEach(e => e.y > canvas.height ? limit++ : limit--)
+            if(limit == this.amount)
+                cancelAnimationFrame(gid)
         }
         update() {
             this.particlesArray.forEach(e => e.update())
         }
     }
-    const img = new Image();
-    img.src = 'tbh.png';
 
-    let globalID;
+
     function animate() {
-        cancelAnimationFrame(globalID)
+        gid = requestAnimationFrame(animate)
         ctx.clearRect(0, 0, canvas.width, canvas.height)
-        ctx.drawImage(img, canvas.width / 2 - 500 / 2, canvas.height / 2 - 500 / 2);
+        if(special_color == "50%")
+            ctx.drawImage(img, canvas.width / 2 - img.width / 2, canvas.height / 2 - img.height / 2);
         effect.draw(ctx);
         effect.update();
-        globalID = requestAnimationFrame(animate);
     }
 
     const effect = new Effect(canvas.width, canvas.height, 1000);
     animate();
-    setTimeout(() => { cancelAnimationFrame(globalID) }, 4000);
 }
 
 function yippie() {
     new Audio('yippie.mp3').play();
+    if(gid) {
+        cancelAnimationFrame(gid);
+        gid = null;
+    }
     animation();
+}
 
-
+function yippie_break() {
+    new Audio('yippie_break.mp3').play();
+    if(gid) {
+        cancelAnimationFrame(gid);
+        gid = null;
+    }
+    animation("0%");
 }
